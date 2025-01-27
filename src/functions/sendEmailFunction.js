c21a4227242c22024812a50f679f12aa3e4c7261
unknown <manjurajb@gmail.com>
2025-01-27 14:30:08 +0000

    files modified

--- src/functions/sendEmailFunction.js
+++ src/functions/sendEmailFunction.js
@@ -6,6 +6,8 @@ app.http('sendEmailFunction', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
+        context.log(`Request: ${JSON.stringify(request)}`);
+
         const name = request.query.get('name') || await request.text() || 'world';
 
         try {
@@ -21,9 +23,9 @@ app.http('sendEmailFunction', {
                 // Email details
                 let mailOptions = {
                     from: process.env.EMAIL_USER,
-                    to: req.body.to, // Recipient email from request body
-                    subject: req.body.subject || "Azure Function Email",
-                    text: req.body.message || "Hello from Azure Function!"
+                    to: request.body.email, // Recipient email from request body
+                    subject: request.body.subject || "Azure Function Email",
+                    text: request.body.message || "Hello from Azure Function!"
                 };
         
                 // Send email
